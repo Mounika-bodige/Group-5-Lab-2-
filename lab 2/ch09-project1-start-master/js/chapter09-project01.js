65f41169922ea4a12a9de2201fa90c38aa92e77b
Mounika-bodige <65857924+Mounika-bodige@users.noreply.github.com>
2021-01-25 11:41:28 -0500

    Add files via upload

---
+++ lab 2/ch09-project1-start-master/js/chapter09-project01.js
@@ -0,0 +1,19 @@
+function focusFunction() {
+  document.getElementById('title').style.background = "yellow";
+  document.getElementById('description').style.background = "yellow";
+  document.getElementById('sub').style.background = "yellow";
+  document.getElementById('year').style.background = "yellow";
+  document.getElementById('medium').style.background = "yellow";
+    document.getElementById('museum').style.background = "yellow";
+
+}
+function blurFunction() {
+  document.getElementById('title').style.background = "";
+  document.getElementById('description').style.background = "";
+  document.getElementById('sub').style.background = "";
+  document.getElementById('year').style.background = "";
+  document.getElementById('medium').style.background = "";
+    document.getElementById('museum').style.background = "";
+
+}
+
